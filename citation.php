4e3855238ec15ddc11e57abc34f9b4c77c51f48a
mynameiszeke <ezekieliyanobor@gmail.com>
2018-03-07 12:41:56 -0600

    Initial commit

---
+++ citation.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ *Ezekiel Iyanobor
+ */
+
+require("libs/config.php");
+$page = easy_decrypt($_GET["id"]);
+$pageDetails = getPageDetailsByName($page);
+include("header.php");
+?>
+<div class="row main-row">
+    <div class="8u">
+        <section class="left-content">
+             <h2><?php echo stripslashes($pageDetails["page_title"]); ?></h2>
+            <?php echo stripslashes($pageDetails["page_desc"]); ?>
+        </section>
+    <head>
+ <meta charset = "UTF-8">
+ <title>citation.php</title>
+ <style type = "text/css">
+  table, th, td {border: 1px solid black};
+ </style>
+ </head>
+ <body>
+ <p>
+ <?php
+  try {
+  $con= new PDO('mysql:host=localhost;dbname=chemtox', "root", "");
+  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $query = "SELECT * FROM citation";
+  //first pass just gets the column names
+  print "<table>";
+  $result = $con->query($query);
+  //return only the first row (we only need field names)
+  $row = $result->fetch(PDO::FETCH_ASSOC);
+  print " <tr>";
+  foreach ($row as $field => $value){
+   print " <th>$field</th>";
+  } // end foreach
+  print " </tr>";
+  //second query gets the data
+  $data = $con->query($query);
+  $data->setFetchMode(PDO::FETCH_ASSOC);
+  foreach($data as $row){
+   print " <tr>";
+   foreach ($row as $name=>$value){
+   print " <td>$value</td>";
+   } // end field loop
+   print " </tr>";
+  } // end record loop
+  print "</table>";
+  } catch(PDOException $e) {
+   echo 'ERROR: ' . $e->getMessage();
+  } // end try
+ ?>
+ </p>
+ </body>
+    </div>
+  
+</div>
+<?php
+include("footer.php");
+?>
